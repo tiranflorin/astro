1e92f9930a5d5128a3d3e33f0a75d31c5fed88a6
Stackbit <projects@stackbit.com>
2019-09-30 06:18:37 +0000

    initial commit by Stackbit

---
+++ index.md
@@ -0,0 +1,14 @@
+---
+title: Home
+sections:
+  - section_id: intro
+    component: intro.html
+    type: intro
+    title: Intro
+    content: 'Web developer, tinker, amateur astronomer'
+menus:
+  main:
+    title: Home
+    weight: 1
+layout: home
+---
---
+++ stackbit-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+if [[ -z "${STACKBIT_API_KEY}" ]]; then
+    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
+else
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5d919e1352ba610011d3f336 
+fi
+jekyll build
